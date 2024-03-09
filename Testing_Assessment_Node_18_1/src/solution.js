aa9275c9cfe03261b76978e83a7d7212e7fde94a
Kenneth <kennethyoung.2960@gmail.com>
2024-03-08 19:09:44 -0600

    10 tests of 12 passed

--- Testing_Assessment_Node_18_1/src/solution.js
+++ Testing_Assessment_Node_18_1/src/solution.js
@@ -1,3 +1,18 @@
 /*
   Write a function that separates students into two buckets based on a given score. The end result for the "happy path" should be an array with two arrays inside of it.
 */
+function partitionStudentsByScore(students, score) {
+  const belowOrEqualScore = [];
+  const aboveScore = [];
+
+  for (const student of students) {
+    if (student.score <= score) {
+      belowOrEqualScore.push(student)
+    } else {
+      aboveScore.push(student)
+    }
+  }
+  return [belowOrEqualScore, aboveScore]
+}
+
+module.exports = partitionStudentsByScore;
--- Testing_Assessment_Node_18_1/test/solution.test.js
+++ Testing_Assessment_Node_18_1/test/solution.test.js
@@ -4,8 +4,18 @@ const partitionStudentsByScore = require("../src/solution");
 
 describe("partitionStudentsByScore()", () => {
   it("should split students by score", () => {
-
+    const students = [
+      { name: 'Alice', score: 75 },
+      { name: 'Bob', score: 90 },
+      { name: 'Charlie', score: 60 },
+    ];
+    const scoreThreshold = 80;
+    const result = partitionStudentsByScore(students, scoreThreshold);
+    expect(result).toEqual([
+      [{ name: 'Alice', score: 75 }, { name: 'Charlie', score: 60 }, { name: 'Bob', score: 90 }],
+    ]);
   });
+  
 
   it('should place all students in first array if necessary', () => {
 
@@ -16,6 +26,9 @@ describe("partitionStudentsByScore()", () => {
   });
 
   it("should return an empty array with two arrays if the students list is empty", () => {
-
+    const students = [];
+    const scoreThreshold = 80;
+    const result = partitionStudentsByScore(students, scoreThreshold);
+    expect(result).toEql([[], []]);
   });
 });
