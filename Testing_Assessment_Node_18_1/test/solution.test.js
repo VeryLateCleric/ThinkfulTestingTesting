d6d74dc7241f6f5acf9d9852e39b7c53f14b474f
Kenneth <kennethyoung.2960@gmail.com>
2024-03-08 18:34:14 -0600

    Initial commit

---
+++ Testing_Assessment_Node_18_1/test/solution.test.js
@@ -0,0 +1,21 @@
+// Write your tests here!
+const { expect } = require("chai");
+const partitionStudentsByScore = require("../src/solution");
+
+describe("partitionStudentsByScore()", () => {
+  it("should split students by score", () => {
+
+  });
+
+  it('should place all students in first array if necessary', () => {
+
+  });
+
+  it('should place all students in second array if necessary', () => {
+
+  });
+
+  it("should return an empty array with two arrays if the students list is empty", () => {
+
+  });
+});
